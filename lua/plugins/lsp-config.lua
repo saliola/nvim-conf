df31343827469d3675df1db57d3cf9ad75515022
Franco Saliola <saliola@gmail.com>
2024-04-20 10:38:04 -0400

    remove tabs...

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,44 +1,44 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "texlab" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "texlab" },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
 
-			lspconfig.lua_ls.setup({})
+            lspconfig.lua_ls.setup({})
 
-			lspconfig.texlab.setup({
-				flags = {
-					debounce_text_changes = 150,
-				},
-				settings = {
-					texlab = {
-						chktex = {
-							onEdit = true,
-							onOpenAndSave = true,
-						},
-					},
-				},
-			})
+            lspconfig.texlab.setup({
+                flags = {
+                    debounce_text_changes = 150,
+                },
+                settings = {
+                    texlab = {
+                        chktex = {
+                            onEdit = true,
+                            onOpenAndSave = true,
+                        },
+                    },
+                },
+            })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>ka", vim.diagnostic.open_float, {})
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
-		end,
-	},
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "<leader>ka", vim.diagnostic.open_float, {})
+            vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
+            vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
+        end,
+    },
 }
