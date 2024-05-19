32566b14dfdd2cef16701b66f1d173916bc5f2df
Franco Saliola <saliola@gmail.com>
2024-05-19 08:20:37 -0400

    installl indent-blankline.nvim

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        'lukas-reineke/indent-blankline.nvim',
+        main = 'ibl',
+        event = 'VeryLazy',
+        opts = {
+            scope = {
+                show_start = false,
+                show_end = false,
+            },
+        },
+    },
+}
+
