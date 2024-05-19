5c8a97b931d290d40c7a0d3a59a5aeb5778545ee
Franco Saliola <saliola@gmail.com>
2024-05-19 10:18:47 -0400

    disable key mappings for nvim-snippets

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -8,49 +8,49 @@ return {
             friendly_snippets = true,
             create_cmp_source = true,
         },
-        keys = {
-            {
-              "<Tab>",
-              function()
-                if vim.snippet.jumpable(1) then
-                  vim.schedule(function()
-                    vim.snippet.jump(1)
-                  end)
-                  return
-                end
-                return "<Tab>"
-              end,
-              expr = true,
-              silent = true,
-              mode = "i",
-            },
-            {
-              "<Tab>",
-              function()
-                vim.schedule(function()
-                  vim.snippet.jump(1)
-                end)
-              end,
-              expr = true,
-              silent = true,
-              mode = "s",
-            },
-            {
-              "<S-Tab>",
-              function()
-                if vim.snippet.jumpable(-1) then
-                  vim.schedule(function()
-                    vim.snippet.jump(-1)
-                  end)
-                  return
-                end
-                return "<S-Tab>"
-              end,
-              expr = true,
-              silent = true,
-              mode = { "i", "s" },
-            },
-        },
+        -- keys = {
+            -- {
+            --   "<Tab>",
+            --   function()
+            --     if vim.snippet.jumpable(1) then
+            --       vim.schedule(function()
+            --         vim.snippet.jump(1)
+            --       end)
+            --       return
+            --     end
+            --     return "<Tab>"
+            --   end,
+            --   expr = true,
+            --   silent = true,
+            --   mode = "i",
+            -- },
+            -- {
+            --   "<Tab>",
+            --   function()
+            --     vim.schedule(function()
+            --       vim.snippet.jump(1)
+            --     end)
+            --   end,
+            --   expr = true,
+            --   silent = true,
+            --   mode = "s",
+            -- },
+            -- {
+            --   "<S-Tab>",
+            --   function()
+            --     if vim.snippet.jumpable(-1) then
+            --       vim.schedule(function()
+            --         vim.snippet.jump(-1)
+            --       end)
+            --       return
+            --     end
+            --     return "<S-Tab>"
+            --   end,
+            --   expr = true,
+            --   silent = true,
+            --   mode = { "i", "s" },
+            -- },
+        -- },
     },
     {
         "hrsh7th/nvim-cmp",
