2a48b25221d2f935ed26106ece852a82527b25d0
Franco Saliola <saliola@gmail.com>
2024-05-19 08:21:04 -0400

    bqf & pqf for better & prettier quickfix

---
+++ lua/plugins/quickfix.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        'kevinhwang91/nvim-bqf',
+        ft = 'qf',
+    },
+
+    {
+        'yorickpeterse/nvim-pqf',
+        event = 'VeryLazy',
+        opts = {
+            show_multiple_lines = true,
+            max_filename_length = 40,
+        },
+    },
+}
