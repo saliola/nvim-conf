3adab1c85d3a05d0a85b40071ab83023b0357a2e
Franco Saliola <saliola@gmail.com>
2024-06-08 10:14:19 -0400

    update folding settings

--- lua/folding.lua
+++ lua/folding.lua
@@ -1,15 +1,12 @@
--- folding
-vim.keymap.set("n", "<leader><space>", "za", opts) -- toggle fold
-vim.cmd([[nnoremap <expr> } foldclosed(search('^$', 'Wn')) == -1 ? "}" : "}j}"]]) -- skip over closed folds with { and }
-vim.cmd([[nnoremap <expr> { foldclosed(search('^$', 'Wnb')) == -1 ? "{" : "{k{"]])
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+-- enable folding
 vim.opt.foldcolumn = "0"
-vim.opt.foldtext = ""
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 0
+vim.opt.foldmethod = "expr"
+vim.opt.foldtext = ""
 
--- Python folding
+-- customize Python folding
 local query = require("vim.treesitter.query")
 query.set("python", "folds", [[
     [
@@ -22,3 +19,12 @@ query.set("python", "folds", [[
       (import_from_statement)
     ]+ @fold
 ]])
+
+-- disable Lua folding
+local query = require("vim.treesitter.query")
+query.set("lua", "folds", [[]])
+
+-- keybindings for folding
+vim.keymap.set("n", "<leader><space>", "za", opts) -- toggle fold
+vim.cmd([[nnoremap <expr> } foldclosed(search('^$', 'Wn')) == -1 ? "}" : "}j}"]]) -- skip over closed folds with { and }
+vim.cmd([[nnoremap <expr> { foldclosed(search('^$', 'Wnb')) == -1 ? "{" : "{k{"]])
