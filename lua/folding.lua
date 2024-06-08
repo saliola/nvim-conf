17751082d74d8d2a6dc1bb5e10a9816cc822b10f
Franco Saliola <saliola@gmail.com>
2024-06-08 12:42:25 -0400

    tweak display of folds

--- lua/folding.lua
+++ lua/folding.lua
@@ -5,7 +5,7 @@ vim.opt.foldmethod = "expr"
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 1
 vim.opt.foldtext = ""
-vim.cmd([[highlight Folded guibg=#202020]])
+vim.cmd([[highlight Folded guibg=NONE guifg=#555555]])
 
 -- customize Python folding
 local query = require("vim.treesitter.query")
@@ -28,3 +28,8 @@ query.set("lua", "folds", [[]])
 vim.keymap.set("n", "<leader><space>", "za", opts) -- toggle fold
 vim.cmd([[nnoremap <expr> } foldclosed(search('^$', 'Wn')) == -1 ? "}" : "}j}"]]) -- skip over closed folds with { and }
 vim.cmd([[nnoremap <expr> { foldclosed(search('^$', 'Wnb')) == -1 ? "{" : "{k{"]])
+
+-- "Focus" the current line: close all folds; open just the folds containing
+-- the current line; move the line a few lines above the center of the screen.
+-- From Steve Losh.
+vim.keymap.set("n", "<leader>zf", "mzzMzvzz10<c-e>`z", opts)
