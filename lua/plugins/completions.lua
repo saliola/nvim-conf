8165f8e4d98e92dc819c8f2cdae90c75d0af9103
Franco Saliola <saliola@gmail.com>
2024-06-09 12:04:53 -0400

    disable friendly-snippets ... there seems to be a bug

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,56 +1,13 @@
 return {
     {
         "garymjr/nvim-snippets",
-        dependencies = {
-            "rafamadriz/friendly-snippets",
-        },
+        -- dependencies = {
+        --     "rafamadriz/friendly-snippets",
+        -- },
         opts = {
-            friendly_snippets = true,
+            -- friendly_snippets = true,
             create_cmp_source = true,
         },
-        -- keys = {
-            -- {
-            --   "<Tab>",
-            --   function()
-            --     if vim.snippet.jumpable(1) then
-            --       vim.schedule(function()
-            --         vim.snippet.jump(1)
-            --       end)
-            --       return
-            --     end
-            --     return "<Tab>"
-            --   end,
-            --   expr = true,
-            --   silent = true,
-            --   mode = "i",
-            -- },
-            -- {
-            --   "<Tab>",
-            --   function()
-            --     vim.schedule(function()
-            --       vim.snippet.jump(1)
-            --     end)
-            --   end,
-            --   expr = true,
-            --   silent = true,
-            --   mode = "s",
-            -- },
-            -- {
-            --   "<S-Tab>",
-            --   function()
-            --     if vim.snippet.jumpable(-1) then
-            --       vim.schedule(function()
-            --         vim.snippet.jump(-1)
-            --       end)
-            --       return
-            --     end
-            --     return "<S-Tab>"
-            --   end,
-            --   expr = true,
-            --   silent = true,
-            --   mode = { "i", "s" },
-            -- },
-        -- },
     },
     {
         "hrsh7th/nvim-cmp",
@@ -76,7 +33,7 @@ return {
                 mapping = cmp.mapping.preset.insert({
                     ['<C-o>'] = cmp.mapping.complete(),
                     ['<C-e>'] = cmp.mapping.abort(),
-                    ['<CR>'] = cmp.mapping.confirm({ select = true })
+                    ['<CR>'] = cmp.mapping.confirm({ select = false })
                 }),
                 sources = cmp.config.sources({
                     { name = "buffer" },
