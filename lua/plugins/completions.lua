200f3a134ddc70e9ef00dd614c193a27682ef53a
Franco Saliola <saliola@gmail.com>
2024-06-09 14:39:52 -0400

    get snippets working for latex!

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,11 +1,51 @@
 return {
     {
         "garymjr/nvim-snippets",
-        -- dependencies = {
-        --     "rafamadriz/friendly-snippets",
-        -- },
+        keys = {
+            {
+                "<Tab>",
+                function()
+                    if vim.snippet.active({ direction = 1 }) then
+                        vim.schedule(function()
+                            vim.snippet.jump(1)
+                        end)
+                        return
+                    end
+                    return "<Tab>"
+                end,
+                expr = true,
+                silent = true,
+                mode = "i",
+            },
+            {
+                "<Tab>",
+                function()
+                    vim.schedule(function()
+                        vim.snippet.jump(1)
+                    end)
+                end,
+                expr = true,
+                silent = true,
+                mode = "s",
+            },
+            {
+                "<S-Tab>",
+                function()
+                    if vim.snippet.active({ direction = -1 }) then
+                        vim.schedule(function()
+                            vim.snippet.jump(-1)
+                        end)
+                        return
+                    end
+                    return "<S-Tab>"
+                end,
+                expr = true,
+                silent = true,
+                mode = { "i", "s" },
+            },
+        },
         opts = {
-            -- friendly_snippets = true,
+            friendly_snippets = false,
             create_cmp_source = true,
         },
     },
@@ -31,8 +71,6 @@ return {
                     documentation = cmp.config.window.bordered(),
                 },
                 mapping = cmp.mapping.preset.insert({
-                    ['<C-o>'] = cmp.mapping.complete(),
-                    ['<C-e>'] = cmp.mapping.abort(),
                     ['<CR>'] = cmp.mapping.confirm({ select = false })
                 }),
                 sources = cmp.config.sources({
