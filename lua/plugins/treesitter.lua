ff9a0ba32437cf13a0f632354f092737d1314519
Franco Saliola <saliola@gmail.com>
2024-06-20 09:02:45 -0400

    install treesitter-context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,27 +1,32 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local configs = require("nvim-treesitter.configs")
-        configs.setup({
-            highlight = {
-                enable = true
-            },
-            indent = {
-                enable = true
-            },
-            ensure_installed = {
-                "c",
-                "html",
-                "javascript",
-                "latex",
-                "lua",
-                "markdown",
-                "python",
-                "vim",
-                "vimdoc",
-            },
-            sync_install = false,
-        })
-    end,
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+            configs.setup({
+                highlight = {
+                    enable = true
+                },
+                indent = {
+                    enable = true
+                },
+                ensure_installed = {
+                    "c",
+                    "html",
+                    "javascript",
+                    "latex",
+                    "lua",
+                    "markdown",
+                    "python",
+                    "vim",
+                    "vimdoc",
+                },
+                sync_install = false,
+            })
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+    }
 }
