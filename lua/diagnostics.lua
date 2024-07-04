4f2a6fe8653d89226f6bc690d1c635cb09c1fa98
Franco Saliola <saliola@gmail.com>
2024-07-04 07:32:40 -0400

    config diagnostics to use different icons / symbols

--- lua/diagnostics.lua
+++ lua/diagnostics.lua
@@ -1,12 +1,23 @@
 vim.diagnostic.config({
     virtual_text = false,
+    underline = false,
+    update_in_insert = false,
+    severity_sort = true,
     float = {
         border = "rounded",
         source = "always",
     },
 })
 
+-- configure the underline style
 local hl_groups = { 'DiagnosticUnderlineError' }
 for _, hl in ipairs(hl_groups) do
     vim.cmd.highlight(hl .. ' gui=undercurl')
+end
+
+-- change symbols for DiagnosticSign
+local symbols = { Error = "󰅙", Info = "󰋼", Hint = "󰌵", Warn = "" }
+for name, icon in pairs(symbols) do
+    local hl = "DiagnosticSign" .. name
+    vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
 end
