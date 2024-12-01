a08309f2874dd87c8ffb3a6d228559440ecab30c
Franco Saliola <saliola@gmail.com>
2024-11-30 22:48:58 -0800

    install github-copilot

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,5 +1,8 @@
 return {
     {
+        "github/copilot.vim"
+    },
+    {
         "garymjr/nvim-snippets",
         keys = {
             {
@@ -70,6 +73,10 @@ return {
                     completion = cmp.config.window.bordered(),
                     documentation = cmp.config.window.bordered(),
                 },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+                }),
                 sources = cmp.config.sources({
                     { name = "snippets" },
                     { name = "buffer" },
