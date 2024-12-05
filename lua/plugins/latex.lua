17de26516d960fb7414ac9355754453cbc84d2fb
Franco Saliola <saliola@gmail.com>
2024-12-05 17:57:24 -0500

    testing knap ...

---
+++ lua/plugins/latex.lua
@@ -0,0 +1,22 @@
+return {
+    {
+        "lervag/vimtex",
+        init = function()
+            vim.g.vimtex_view_method = 'skim'
+            vim.g.vimtex_compiler_latexmk = {
+                options = {
+                    "-shell-escape",
+                    "-verbose",
+                    "-file-line-error",
+                    "-synctex=1",
+                    "-interaction=nonstopmode",
+                },
+                out_dir = '/tmp/latex-output-directory',
+            }
+        end,
+    },
+
+    {
+        "frabjous/knap"
+    }
+}
