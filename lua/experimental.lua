6a411b331da251acdb3d4550ae8c57aa2d26726e
Franco Saliola <saliola@gmail.com>
2024-12-08 22:08:40 -0500

    tweak 'sage copy' command

--- lua/experimental.lua
+++ lua/experimental.lua
@@ -1,13 +1,22 @@
-function copy_sage_output(pane_id, sage_command)
-    vim.system({ 'tmux', 'copy-mode', '-t', pane_id })
-    vim.system({ 'tmux', 'send', '-t', pane_id, 'k$' })
-    vim.system({ 'tmux', 'send', '-t', pane_id, '-X', 'begin-selection' })
-    if sage_command then
-        vim.system({ 'tmux', 'send', '-t', pane_id, '-X', 'search-backward', '^sage:' })
-    else
-        vim.system({ 'tmux', 'send', '-t', pane_id, '-X', 'search-backward', '^(sage|\\.\\.\\.\\.):' })
-        vim.system({ 'tmux', 'send', '-t', pane_id, 'j0' })
+local pane_id = 'right'
+
+function copy_sage_output(output_only)
+    if output_only == nil then
+        output_only = true
     end
-    vim.system({ 'tmux', 'send', '-t', pane_id, '-X', 'copy-selection-and-cancel' })
+    vim.system({ 'tmux', 'copy-mode', '-t', pane_id }):wait()
+    vim.system({ 'tmux', 'send', '-t', pane_id, 'k$' }):wait()
+    vim.system({ 'tmux', 'send', '-t', pane_id, '-X', 'begin-selection' }):wait()
+    if output_only then
+        vim.system({ 'tmux', 'send', '-t', pane_id, '-X', 'search-backward', '^(sage|\\.\\.\\.\\.):' }):wait()
+        vim.system({ 'tmux', 'send', '-t', pane_id, 'j0' }):wait()
+    else
+        vim.system({ 'tmux', 'send', '-t', pane_id, '-X', 'search-backward', '^sage:' }):wait()
+    end
+    vim.system({ 'tmux', 'send', '-t', pane_id, '-X', 'copy-selection-and-cancel' }):wait()
+end
+
+function copy_paste_sage_output(output_only)
+    copy_paste_sage_output(output_only)
     vim.cmd("Tput")
 end
