752011962291ea49fb84049b4edd85fd7f7a66fe
Franco Saliola <saliola@gmail.com>
2024-12-08 22:27:24 -0500

    switch from github/copilot.vim to zbirenbaum/copilot.lua

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,6 +1,16 @@
 return {
     {
-        "github/copilot.vim"
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            require("copilot").setup({
+                suggestion = {
+                    enabled = true,
+                    auto_trigger = true,
+                }
+            })
+        end,
     },
     {
         "garymjr/nvim-snippets",
