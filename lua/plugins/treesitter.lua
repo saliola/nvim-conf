82b4468a16b9fa9680fef2688241d5b7a6e62484
Franco Saliola <saliola@gmail.com>
2024-12-13 16:54:34 -0500

    install treewalker

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -29,5 +29,17 @@ return {
     },
     {
         "nvim-treesitter/nvim-treesitter-context",
+    },
+    {
+        "aaronik/treewalker.nvim",
+        config = function ()
+            vim.api.nvim_set_keymap('n', 'tj', ':Treewalker Down<CR>', { noremap = true, silent = true })
+            vim.api.nvim_set_keymap('n', 'tk', ':Treewalker Up<CR>', { noremap = true, silent = true })
+            vim.api.nvim_set_keymap('n', 'th', ':Treewalker Left<CR>', { noremap = true, silent = true })
+            vim.api.nvim_set_keymap('n', 'tl', ':Treewalker Right<CR>', { noremap = true, silent = true })
+            require('treewalker').setup({
+                highlight = true,
+            })
+        end
     }
 }
