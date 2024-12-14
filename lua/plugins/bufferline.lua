d9b7f7334d31aac97e83841afa5b0b67da457aea
Franco Saliola <saliola@gmail.com>
2024-12-14 18:01:09 -0500

    revert to bufferline

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,11 @@
+return {
+    "akinsho/bufferline.nvim",
+    -- version = "*",
+    branch = "main",
+    opts = {
+        options = {
+            mode = "tabs",
+            separator_stype = "slant",
+        },
+    },
+}
