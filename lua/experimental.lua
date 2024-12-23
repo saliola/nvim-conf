0e5d1430e253fb88ae59b951b12999a533623072
Franco Saliola <saliola@gmail.com>
2024-12-23 00:43:19 -0500

    TmuxPut and TmuxYank lua functions

--- lua/experimental.lua
+++ lua/experimental.lua
@@ -1,3 +1,43 @@
+--[ utility functions ]--
+
+function normal_mode()    
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<esc>", true, false, true), 'x', true)
+end
+
+
+-- [ tmux yank/put ]--
+function TmuxPut()
+    local tempfile = vim.fn.tempname()
+    vim.system({ 'tmux', 'save-buffer', tempfile }):wait()
+    vim.cmd("read " .. tempfile)
+end
+
+vim.keymap.set("n", "<leader>tp", TmuxPut)
+
+
+function TmuxYank()
+    local tempfile = vim.fn.tempname()
+    local range
+    local mode = vim.api.nvim_get_mode().mode
+
+    if vim.api.nvim_get_mode().mode == 'n' then
+        range = '.'
+    else
+        -- Escape to normal mode
+        normal_mode()
+        range = "'<,'>"
+    end
+
+    vim.cmd(range .. "write!" .. tempfile)
+    vim.system({ 'tmux', 'load-buffer', tempfile }):wait()
+end
+
+vim.keymap.set('v', "<leader>ty", TmuxYank, { noremap = true, silent = true })
+vim.keymap.set('n', "<leader>ty", TmuxYank, { noremap = true })
+
+
+-- [ Sage copy/paste ]--
+
 local pane_id = 'right'
 
 function copy_sage_output(output_only)
@@ -16,9 +56,15 @@ function copy_sage_output(output_only)
     vim.system({ 'tmux', 'send', '-t', pane_id, '-X', 'copy-selection-and-cancel' }):wait()
 end
 
+
 function copy_paste_sage_output(output_only)
     copy_sage_output(output_only)
-    vim.cmd("Tput")
+    local current_line = vim.api.nvim_get_current_line()
+    local leading_spaces = #string.match(current_line, "^%s*")
+    local shiftwidth = vim.api.nvim_buf_get_option(0, 'shiftwidth')
+    local indent_level = leading_spaces / shiftwidth
+    TmuxPut()
+    vim.cmd("'[,']" .. string.rep(">", indent_level))
 end
 
 vim.keymap.set("n", "<leader>sc", copy_paste_sage_output)
