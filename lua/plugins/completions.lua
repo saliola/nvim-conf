36618eb0da4898224970648af6d910d7b7178656
Franco Saliola <saliola@gmail.com>
2025-01-03 17:57:00 -0500

    switch to blink.cmp

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -12,88 +12,60 @@ return {
             })
         end,
     },
-    {
-        "garymjr/nvim-snippets",
-        keys = {
-            {
-                "<Tab>",
-                function()
-                    if vim.snippet.active({ direction = 1 }) then
-                        vim.schedule(function()
-                            vim.snippet.jump(1)
-                        end)
-                        return
-                    end
-                    return "<Tab>"
-                end,
-                expr = true,
-                silent = true,
-                mode = "i",
-            },
-            {
-                "<Tab>",
-                function()
-                    vim.schedule(function()
-                        vim.snippet.jump(1)
-                    end)
-                end,
-                expr = true,
-                silent = true,
-                mode = "s",
-            },
-            {
-                "<S-Tab>",
-                function()
-                    if vim.snippet.active({ direction = -1 }) then
-                        vim.schedule(function()
-                            vim.snippet.jump(-1)
-                        end)
-                        return
-                    end
-                    return "<S-Tab>"
-                end,
-                expr = true,
-                silent = true,
-                mode = { "i", "s" },
-            },
-        },
-        opts = {
-            friendly_snippets = false,
-            create_cmp_source = true,
-        },
-    },
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-cmdline",
-        },
-        config = function()
-            local cmp = require("cmp")
 
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        vim.snippet.expand(args.body)
-                    end,
+    {
+        'saghen/blink.cmp',
+        dependencies = 'rafamadriz/friendly-snippets',
+        version = '*',
+        opts = {
+            keymap = { preset = 'default' },
+            appearance = {
+                use_nvim_cmp_as_default = true,
+                nerd_font_variant = 'mono'
+            },
+        },
+        sources = {
+            default = { 'lsp', 'path', 'snippets', 'buffer' },
+            providers = {
+                buffer = {
+                    name = "buffer",
+                    module = "blink.cmp.sources.buffer",
+                    opts = {
+                        get_bufnrs = function()
+                            return vim
+                                .iter(vim.api.nvim_list_wins())
+                                :map(function(win) return vim.api.nvim_win_get_buf(win) end)
+                                :filter(function(buf) return vim.bo[buf].buftype ~= 'nofile' end)
+                                :totable()
+                        end,
+                    }
                 },
-                window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
+                lsp = {
+                    name = "lsp",
+                    enabled = true,
+                    module = "blink.cmp.sources.lsp",
+                    kind = "LSP",
+                    score_offset = 1000,
                 },
-                mapping = cmp.mapping.preset.insert({
-                    ['<C-Space>'] = cmp.mapping.complete(),
-                    ['<CR>'] = cmp.mapping.confirm({ select = false }),
-                }),
-                sources = cmp.config.sources({
-                    { name = "snippets" },
-                    { name = "buffer" },
-                    { name = "path" },
-                    { name = "nvim_lsp" },
-                }),
-            })
-        end,
+                snippets = {
+                    name = "snippets",
+                    enabled = true,
+                    module = "blink.cmp.sources.snippets",
+                    score_offset = 900,
+                },
+                copilot = {
+                    name = "copilot",
+                    enabled = true,
+                    module = "blink-cmp-copilot",
+                    kind = "Copilot",
+                    score_offset = -100,
+                    async = true,
+                },
+            },
+        },
+        signature = {
+            enabled = true,
+        },
+        opts_extend = { "sources.default" }
     },
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,19 +1,5 @@
 return {
     {
-        "nvimtools/none-ls.nvim",
-        config = function()
-            local null_ls = require("null-ls")
-            null_ls.setup({
-                sources = {
-                    null_ls.builtins.formatting.stylua,
-                },
-            })
-
-            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-        end,
-    },
-
-    {
         "williamboman/mason.nvim",
         config = function()
             require("mason").setup()
@@ -50,10 +36,21 @@ return {
     {
         "neovim/nvim-lspconfig",
         lazy = false,
+        dependencies = {
+            { 'saghen/blink.cmp', },
+            {
+                "folke/lazydev.nvim",
+                ft = "lua",
+                opts = {
+                    library = {
+                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                    },
+                },
+            },
+        },
         config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-            local lspconfig = require("lspconfig")
+            local capabilities = require('blink.cmp').get_lsp_capabilities()
+            local lspconfig = require('lspconfig')
 
             lspconfig.bashls.setup({
                 capabilities = capabilities,
