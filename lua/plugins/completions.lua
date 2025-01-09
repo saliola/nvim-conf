c2805fd6c424c4d0047d6c6155538299c1ea5a5c
Franco Saliola <saliola@gmail.com>
2025-01-09 00:18:12 -0500

    tweaking blink.cmp settings (& trying to install the dictionary and emoji sources)

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -18,56 +18,154 @@ return {
         'saghen/blink.cmp',
         dependencies = {
             { 'rafamadriz/friendly-snippets', },
+            -- { 'moyiz/blink-emoji.nvim', },
+            -- { 'Kaiser-Yang/blink-cmp-dictionary', },
         },
         version = '*',
         opts = {
-            keymap = { preset = 'default' },
-            appearance = {
-                use_nvim_cmp_as_default = true,
-                nerd_font_variant = 'mono'
+            keymap = {
+                preset = 'default',
+                ["<C-space>"] = {
+                    "select_and_accept",
+                },
+                ['<Tab>'] = {
+                    function(cmp)
+                        if cmp.snippet_active() then
+                            return cmp.accept()
+                        else
+                            return cmp.select_next()
+                        end
+                    end,
+                    'snippet_forward',
+                    'fallback'
+                },
+                ['<S-Tab>'] = {
+                    function(cmp)
+                        if cmp.snippet_active() then
+                            return cmp.accept()
+                        else
+                            return cmp.select_prev()
+                        end
+                    end,
+                    'snippet_backward',
+                    'fallback'
+                },
+                cmdline = {
+                    preset = 'default',
+                    ["<Tab>"] = {
+                        'select_next',
+                        'fallback'
+                    },
+                    ["<S-Tab>"] = {
+                        'select_prev',
+                        'fallback'
+                    },
+                },
             },
             completion = {
-                list = {
-                    selection = "auto_insert",
-                },
                 accept = {
                     auto_brackets = { enabled = false },
                 },
-            },
-        },
-        sources = {
-            default = { 'lsp', 'path', 'snippets', 'buffer' },
-            providers = {
-                buffer = {
-                    name = "buffer",
-                    module = "blink.cmp.sources.buffer",
-                    opts = {
-                        get_bufnrs = function()
-                            return vim
-                                .iter(vim.api.nvim_list_wins())
-                                :map(function(win) return vim.api.nvim_win_get_buf(win) end)
-                                :filter(function(buf) return vim.bo[buf].buftype ~= 'nofile' end)
-                                :totable()
-                        end,
-                    }
+                list = {
+                    selection = {
+                        auto_insert = function(ctx) return ctx.mode ~= 'normal' end,
+                        preselect = function(ctx) return ctx.mode ~= 'cmdline' end,
+                    },
                 },
-                lsp = {
-                    name = "lsp",
-                    enabled = true,
-                    module = "blink.cmp.sources.lsp",
-                    kind = "LSP",
-                    score_offset = 1000,
+                menu = {
+                    draw = {
+                        columns = {
+                            { "kind_icon" },
+                            { "label", "label_description", gap = 1 },
+                            { "source_name" },
+                        },
+                        components = {
+                            source_name = {
+                                text = function(ctx)
+                                    return '[' .. ctx.source_name .. ']'
+                                end,
+                            },
+                        },
+                    },
                 },
-                snippets = {
-                    name = "snippets",
-                    enabled = true,
-                    module = "blink.cmp.sources.snippets",
-                    score_offset = 900,
+                documentation = {
+                    auto_show = true,
+                    auto_show_delay_ms = 0,
                 },
             },
         },
         signature = {
             enabled = true,
+            window = { border = 'rounded' },
+        },
+        sources = {
+            default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev', 'emoji', 'dictionary' },
+            providers = {
+                buffer = {
+                    name = "buffer",
+                    module = "blink.cmp.sources.buffer",
+                    score_offset = 500,
+                },
+                -- dictionary = {
+                --     name = "dictionary",
+                --     module = "blink-cmp-dictionary",
+                --     opts = {
+                --         prefix_min_len = 3,
+                --         get_command = {
+                --             "rg",
+                --             "--color=never",
+                --             "--no-line-number",
+                --             "--no-messages",
+                --             "--no-filename",
+                --             "--ignore-case",
+                --             "--",
+                --             "${prefix}",
+                --             vim.fn.expand("~/.config/nvim/words"),
+                --         },
+                --     },
+                -- },
+                -- emoji = {
+                --     module = "blink-emoji",
+                --     name = "emoji",
+                --     score_offset = 15,
+                --     opts = { insert = true },
+                -- },
+                -- lazydev = {
+                --     name = "lazydev",
+                --     module = "lazydev.integrations.blink",
+                --     score_offset = 100,
+                -- },
+                lsp = {
+                    name = "lsp",
+                    enabled = true,
+                    module = "blink.cmp.sources.lsp",
+                    kind = "LSP",
+                    fallback = { "snippets", "buffer" },
+                    score_offset = 1000,
+                },
+                path = {
+                    name = "path",
+                    enabled = true,
+                    module = "blink.cmp.sources.path",
+                    score_offset = 800,
+                    opts = {
+                        trailing_slash = false,
+                        label_trailing_slash = true,
+                        get_cwd = function(ctx)
+                            return vim.fn.expand(("#%d:p:h"):format(ctx.bufnr))
+                        end,
+                        show_hidden = false,
+                    },
+                },
+                snippets = {
+                    name = "snippets",
+                    enabled = true,
+                    max_items = 8,
+                    min_keyword_length = 2,
+                    module = "blink.cmp.sources.snippets",
+                    score_offset = 900,
+                },
+            },
         },
         opts_extend = { "sources.default" }
     },
