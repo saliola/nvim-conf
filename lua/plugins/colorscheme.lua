5699a5aa303140b80eca142df8000b5830b60f0f
Franco Saliola <saliola@gmail.com>
2025-01-15 20:37:38 -0500

    change colorscheme to kanagawa

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,7 +7,11 @@ return {
 
     {
         "rebelot/kanagawa.nvim",
+        event = { "VimEnter" },
         lazy = true,
+        config = function()
+            vim.cmd.colorscheme("kanagawa")
+        end,
     },
 
     {
@@ -18,13 +22,12 @@ return {
     {
         "zenbones-theme/zenbones.nvim",
         dependencies = "rktjmp/lush.nvim",
-        event = { "VimEnter" },
         opts = {},
         config = function()
             -- vim.g.zenbones_darken_comments = 45
             -- vim.cmd.colorscheme("zenbones")
             -- vim.cmd.colorscheme("neobones")
-            vim.cmd.colorscheme("kanagawabones")
+            -- vim.cmd.colorscheme("kanagawabones")
         end,
     },
 }
