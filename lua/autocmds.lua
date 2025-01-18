b2caf94105d6fd8fd79d75ac7d5c3a03b8e7d608
Franco Saliola <saliola@gmail.com>
2025-01-18 01:04:15 -0500

    update autocommand to return to last cursor position

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,10 +1,10 @@
-vim.api.nvim_create_autocmd({'BufReadPost', 'BufWritePost'}, {
+vim.api.nvim_create_autocmd({'BufReadPost'}, {
     desc = "Restore cursor position",
     callback = function(args)
         local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
         local line_count = vim.api.nvim_buf_line_count(args.buf)
         if mark[1] > 0 and mark[1] <= line_count then
-            vim.cmd('normal! g`"zz')
+            vim.cmd('normal! g`"zvzz')
         end
     end
 })
