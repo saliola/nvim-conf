69fbd43c7c08123665398373ace7ebfdb2c8099f
Franco Saliola <saliola@gmail.com>
2025-01-18 00:58:46 -0500

    install markview plugin

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,25 @@
+return {
+    {
+        "OXY2DEV/markview.nvim",
+        lazy = false,
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons"
+        },
+        -- config = function()
+        --     require("markview").setup({
+        --         hybrid_modes = { "n" }
+        --     })
+        -- end
+    },
+
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && npm install && git restore .",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    }
+}
