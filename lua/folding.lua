f87894844222cb608350017b4fccba51d69df4ab
Franco Saliola <saliola@gmail.com>
2025-01-19 14:55:04 -0500

    add an autocmd to set foldlevel on BufRead (it was somehow getting overwritten)

--- lua/folding.lua
+++ lua/folding.lua
@@ -2,10 +2,17 @@
 vim.opt.foldcolumn = "0"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldmethod = "expr"
-vim.opt.foldlevel = 99
-vim.opt.foldlevelstart = 1
 vim.opt.foldtext = ""
-vim.cmd([[highlight Folded guibg=#222233 guifg=#555555]])
+vim.api.nvim_set_hl(0, "Folded", { bg = "#222233", fg = "#555555" })
+
+-- HACK: I tried directly setting vim.opt.foldlevel = 99 here
+-- (i.e., without the autocmd), but it somehow got reset to 1
+vim.api.nvim_create_autocmd({'BufRead'}, { desc = "set fold level",
+    callback = function()
+        vim.opt.foldlevel = 99
+        vim.opt.foldlevelstart = 1
+    end
+})
 
 -- customize Python folding
 local query = require("vim.treesitter.query")
