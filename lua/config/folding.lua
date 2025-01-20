452ee082503422eba66b02d92e5d6c3eecd5e1f3
Franco Saliola <saliola@gmail.com>
2025-01-20 16:14:09 -0500

    customize Markdown folding: don't fold lists and code blocks

--- lua/config/folding.lua
+++ lua/config/folding.lua
@@ -1,8 +1,8 @@
 -- enable folding & tweak appearance
 vim.opt.foldcolumn = "0"
-vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.opt.foldmethod = "expr"
 vim.opt.foldtext = ""
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.api.nvim_set_hl(0, "Folded", { bg = "#222233", fg = "#555555" })
 
 -- HACK: I tried directly setting vim.opt.foldlevel = 99 here
@@ -26,4 +26,13 @@ query.set("python", "folds", [[
       (import_statement)
       (import_from_statement)
     ]+ @fold
+]])
+
+-- customize Markdown folding: don't fold lists and code blocks
+local query = require("vim.treesitter.query")
+query.set("markdown", "folds", [[
+    [
+      (section)
+    ] @fold
+      (#trim! @fold)
 ]])
