28632b08cca17d7c2a853fdc94f03b37f662db95
Franco Saliola <saliola@gmail.com>
2025-01-20 17:33:44 -0500

    delete markview.nvim

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,19 +1,5 @@
 return {
     {
-        "OXY2DEV/markview.nvim",
-        lazy = false,
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            "nvim-tree/nvim-web-devicons"
-        },
-        -- config = function()
-        --     require("markview").setup({
-        --         hybrid_modes = { "n" }
-        --     })
-        -- end
-    },
-
-    {
         "iamcco/markdown-preview.nvim",
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
         build = "cd app && npm install && git restore .",
