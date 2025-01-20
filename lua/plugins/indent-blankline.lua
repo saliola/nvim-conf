ea8fd5a51ed4a8cb143aaea36507ef185c4ac41f
Franco Saliola <saliola@gmail.com>
2025-01-19 22:29:43 -0500

    plugin(indent-blankline): change hl settings for IblIndent

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -11,5 +11,9 @@ return {
                 enabled = false,
             },
         },
+        config = function(_, opts)
+            vim.api.nvim_set_hl(0, "IblIndent", { fg = "#222222" })
+            require('ibl').setup(opts)
+        end
     }
 }
