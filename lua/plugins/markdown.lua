3902c25f2e82f2632e92de42aa49413175050dcc
Franco Saliola <saliola@gmail.com>
2025-02-16 17:48:20 -0500

    notes on custom css for markdown preview

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -5,6 +5,7 @@ return {
         build = "cd app && npm install && git restore .",
         init = function()
             vim.g.mkdp_filetypes = { "markdown" }
+            -- vim.g.mkdp_markdown_css = vim.fn.stdpath("data") .. "/path/to/style.css"
         end,
         ft = { "markdown" },
     }
