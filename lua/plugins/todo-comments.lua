8362356d3b903db9cf0c2007b8b037682632f076
Franco Saliola <saliola@gmail.com>
2025-02-16 10:57:01 -0500

    delete barbeque and todo-comments

--- lua/plugins/todo-comments.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    lazy = true,
-    config = function()
-        require("todo-comments").setup()
-    end,
-}
